7629f922f4d5a8b29bead936167470363f80f9e5
learnin <i.learnin@gmail.com>
2019-04-01 00:32:21 +0900

    Fix bug. Cnage to preserve sql upper/lower case when parsing.

--- src/main/java/odata_jdbc/jdbc/SelectSqlParser.java
+++ src/main/java/odata_jdbc/jdbc/SelectSqlParser.java
@@ -10,7 +10,7 @@ public class SelectSqlParser {
 
     public SqlParseResult parse(String sql) throws SQLSyntaxErrorException {
         String oneLineLowerCaseSql = normalize(sql);
-        if (!oneLineLowerCaseSql.startsWith("select ")) {
+        if (!startsWithIgnoreCase(oneLineLowerCaseSql, "select ")) {
             throw new SQLSyntaxErrorException("not supported sql");
         }
         String fromTable = extractFromTable(oneLineLowerCaseSql);
@@ -24,43 +24,46 @@ public class SelectSqlParser {
                 .replaceAll("\\r\\n|\\r|\\n", "\\n") // 改行コードをLFに統一
                 .replaceAll("//.*\\n", "\\n") // 1行コメントを除去
                 .replaceAll("\\n|\\t", " ") // 改行コード、タブをスペースに置換
-                .replaceAll("/\\*.*\\*/", "") // 複数行コメントを除去
-                .toLowerCase();
+                .replaceAll("/\\*.*\\*/", ""); // 複数行コメントを除去
     }
 
     private String extractFromTable(String sql) throws SQLSyntaxErrorException {
-        int fromIndex = sql.indexOf(" from ");
+        int fromIndex = indexOfIgnoreCase(sql, " from ");
         if (fromIndex == -1) {
             throw new SQLSyntaxErrorException();
         }
-        if (sql.indexOf(" from ", fromIndex + 1) != -1) {
+        if (indexOfIgnoreCase(sql, " from ", fromIndex + 1) != -1) {
             throw new SQLSyntaxErrorException("not supported subquery or inline view sql");
         }
         String fromTablePhrase = sql.substring(fromIndex + " from ".length()).trim();
-        int whereIndex = sql.indexOf(" where ");
+        int whereIndex = indexOfIgnoreCase(fromTablePhrase, " where ");
         if (whereIndex != -1) {
             fromTablePhrase = fromTablePhrase.substring(0, whereIndex).trim();
         }
-        int groupByIndex = sql.indexOf(" group by ");
+        int groupByIndex = indexOfIgnoreCase(fromTablePhrase," group by ");
         if (groupByIndex != -1) {
             fromTablePhrase = fromTablePhrase.substring(0, groupByIndex).trim();
         }
-        int havingIndex = sql.indexOf(" having ");
+        int havingIndex = indexOfIgnoreCase(fromTablePhrase," having ");
         if (havingIndex != -1) {
             fromTablePhrase = fromTablePhrase.substring(0, havingIndex).trim();
         }
-        int orderByIndex = sql.indexOf(" order by ");
+        int orderByIndex = indexOfIgnoreCase(fromTablePhrase," order by ");
         if (orderByIndex != -1) {
             fromTablePhrase = fromTablePhrase.substring(0, orderByIndex).trim();
         }
-        if (fromTablePhrase.indexOf(" join ") != -1 || fromTablePhrase.indexOf(",") != -1) {
+        if (indexOfIgnoreCase(fromTablePhrase, " join ") != -1 || fromTablePhrase.indexOf(",") != -1) {
             throw new SQLSyntaxErrorException("not supported join sql");
         }
-        return fromTablePhrase.substring(0, fromTablePhrase.indexOf(" "));
+        int fromTableSpaceIndex = fromTablePhrase.indexOf(" ");
+        if (fromTableSpaceIndex != -1) {
+            return fromTablePhrase.substring(0, fromTableSpaceIndex);
+        }
+        return fromTablePhrase;
     }
 
     private String extractSelectColumnsPhrase(String sql) {
-        int fromIndex = sql.indexOf(" from ");
+        int fromIndex = indexOfIgnoreCase(sql, " from ");
         return sql.substring("select ".length(), fromIndex).trim();
     }
 
@@ -75,12 +78,24 @@ public class SelectSqlParser {
             return results;
         }
         return Arrays.asList(selectColumnsPhrase.split(",")).stream().map(columnPhrase -> {
-            int asIndex = columnPhrase.indexOf(" as ");
+            int asIndex = indexOfIgnoreCase(columnPhrase, " as ");
             if (asIndex == -1) {
                 return new SelectColumn(columnPhrase.trim(), columnPhrase.trim());
             }
             return new SelectColumn(columnPhrase.substring(0, asIndex).trim(), columnPhrase.substring(asIndex + " as ".length()).trim());
         }).collect(Collectors.toList());
+    }
+
+    private boolean startsWithIgnoreCase(String target, String value) {
+        return target.substring(0, value.length()).equalsIgnoreCase(value);
+    }
+
+    private int indexOfIgnoreCase(String target, String value) {
+        return target.toLowerCase().indexOf(value.toLowerCase());
+    }
+
+    private int indexOfIgnoreCase(String target, String value, int fromIndex) {
+        return target.toLowerCase().indexOf(value.toLowerCase(), fromIndex);
     }
 
     public static class SelectColumn {
