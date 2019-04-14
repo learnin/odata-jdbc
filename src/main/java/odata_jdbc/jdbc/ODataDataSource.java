be83bf7aecf9df810f9545896f09941a85c9c294
learnin <i.learnin@gmail.com>
2019-04-14 23:35:08 +0900

    Implementing DataSource

---
+++ src/main/java/odata_jdbc/jdbc/ODataDataSource.java
@@ -0,0 +1,88 @@
+package odata_jdbc.jdbc;
+
+import javax.sql.DataSource;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.logging.Logger;
+
+public class ODataDataSource implements DataSource {
+
+    private String urlScheme = "https";
+    private String serverName;
+    private String databaseName;
+    private String username = "";
+    private String password = "";
+
+    @Override
+    public Connection getConnection() throws SQLException {
+        return getConnection(username, password);
+    }
+
+    @Override
+    public Connection getConnection(String username, String password) throws SQLException {
+        return DriverManager.getConnection(getUrl(), username, password);
+    }
+
+    @Override
+    public <T> T unwrap(Class<T> iface) throws SQLException {
+        if (isWrapperFor(iface)) {
+            return iface.cast(this);
+        }
+        throw new SQLException("this is not instanceof " + iface.getName());
+    }
+
+    @Override
+    public boolean isWrapperFor(Class<?> iface) throws SQLException {
+        return iface.isInstance(this);
+    }
+
+    @Override
+    public PrintWriter getLogWriter() throws SQLException {
+        return null;
+    }
+
+    @Override
+    public void setLogWriter(PrintWriter out) throws SQLException {
+
+    }
+
+    @Override
+    public void setLoginTimeout(int seconds) throws SQLException {
+
+    }
+
+    @Override
+    public int getLoginTimeout() throws SQLException {
+        return 0;
+    }
+
+    @Override
+    public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+        return null;
+    }
+
+    private String getUrl() {
+        return "jdbc:odata-jdbc:" + urlScheme + "://" + serverName + "/" + databaseName + "/";
+    }
+
+    public void setServerName(String serverName) {
+        this.serverName = serverName;
+    }
+
+    public void setDatabaseName(String databaseName) {
+        this.databaseName = databaseName;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+}
--- src/test/java/odata_jdbc/Example.java
+++ src/test/java/odata_jdbc/Example.java
@@ -1,18 +1,23 @@
 package odata_jdbc;
 
+import odata_jdbc.jdbc.ODataDataSource;
 import org.junit.jupiter.api.Test;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class Example {
 
     @Test
-    public void executeSql() throws Exception {
+    public void executeSqlUsingDriverManager() throws Exception {
         try (Connection conn = DriverManager.getConnection("jdbc:odata-jdbc:https://services.odata.org/TripPinRESTierService/", "","")) {
             try (Statement statement = conn.createStatement()) {
                 String sql = "SELECT UserName, FirstName FROM People WHERE FirstName = 'Russell'";
@@ -21,6 +26,50 @@ public class Example {
                     assertEquals("russellwhyte", rs.getString("UserName"));
                     assertFalse(rs.next());
                 }
+            }
+        }
+    }
+
+    @Test
+    public void executeSqlUsingDataSource() throws Exception {
+        System.setProperty(Context.INITIAL_CONTEXT_FACTORY, "org.apache.naming.java.javaURLContextFactory");
+        System.setProperty(Context.URL_PKG_PREFIXES, "org.apache.naming");
+
+        ODataDataSource ods = new ODataDataSource();
+        ods.setServerName("services.odata.org");
+        ods.setDatabaseName("TripPinRESTierService");
+
+        Context context = null;
+        try {
+            context = new InitialContext();
+            context.createSubcontext("java:");
+            context.createSubcontext("java:comp");
+            context.createSubcontext("java:comp/env");
+            context.createSubcontext("java:comp/env/jdbc");
+            context.bind("java:comp/env/jdbc/database", ods);
+        } finally {
+            if (context != null) {
+                context.close();
+            }
+        }
+
+        Context ic = null;
+        try {
+            ic = new InitialContext();
+            DataSource ds = (DataSource) ic.lookup("java:comp/env/jdbc/database");
+            try (Connection conn = ds.getConnection()) {
+                try (Statement statement = conn.createStatement()) {
+                    String sql = "SELECT UserName, FirstName FROM People WHERE FirstName = 'Russell'";
+                    try (ResultSet rs = statement.executeQuery(sql)) {
+                        rs.next();
+                        assertEquals("russellwhyte", rs.getString("UserName"));
+                        assertFalse(rs.next());
+                    }
+                }
+            }
+        } finally {
+            if (ic != null) {
+                ic.close();
             }
         }
     }
