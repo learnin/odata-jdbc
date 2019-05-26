a63d79bc80b737ace40419ebd37e70b7fc1bfdbd
learnin <i.learnin@gmail.com>
2019-05-26 22:19:10 +0900

    Fix bug

--- src/main/java/odata_jdbc/jdbc/ODataUrlBuilder.java
+++ src/main/java/odata_jdbc/jdbc/ODataUrlBuilder.java
@@ -37,21 +37,17 @@ public class ODataUrlBuilder {
         String whereClause = sqlParseResult.whereClause();
         if (whereClause != null && !whereClause.isEmpty()) {
             // Logical Operators
-            // FIXME: id = 'a==' のように値に = とかが入っているとそこまで置換されてしまうので修正必要
-            // -> パース時にWHERE句を1つの文字列ではなく、Ltst<WherePredicate>にすることを考え方が、そこまでしなくても
-            //    以下の置換処理を対象がシングルクォーテーションの外かどうか(置換対象文字以前のシングルクォーテーションを先頭から数えて偶数個か（''は除く））を見ながら置換すればよさそう
-            whereClause = replaceAllExcludeWithinLiteral(whereClause,"!=", "ne");
-            whereClause = replaceAllExcludeWithinLiteral(whereClause,"<>", "ne");
-            whereClause = replaceAllExcludeWithinLiteral(whereClause,"<=", "le");
-            whereClause = replaceAllExcludeWithinLiteral(whereClause,">=", "ge");
-//            whereClause = whereClause.replaceAll("=", "eq");
-            whereClause = replaceAllExcludeWithinLiteral(whereClause,"=", "eq");
-            whereClause = replaceAllExcludeWithinLiteral(whereClause,"<", "lt");
-            whereClause = replaceAllExcludeWithinLiteral(whereClause,">", "gt");
-            whereClause = replaceAllIgnoreCase(whereClause, " AND ", " and ");
-            whereClause = replaceAllIgnoreCase(whereClause, " OR ", " or ");
-            whereClause = replaceAllIgnoreCase(whereClause, " NOT ", " not ");
-            whereClause = replaceAllIgnoreCase(whereClause, " IN ", " in ");
+            whereClause = replaceAllIgnoreCaseExcludeWithinLiteral(whereClause,"!=", "ne");
+            whereClause = replaceAllIgnoreCaseExcludeWithinLiteral(whereClause,"<>", "ne");
+            whereClause = replaceAllIgnoreCaseExcludeWithinLiteral(whereClause,"<=", "le");
+            whereClause = replaceAllIgnoreCaseExcludeWithinLiteral(whereClause,">=", "ge");
+            whereClause = replaceAllIgnoreCaseExcludeWithinLiteral(whereClause,"=", "eq");
+            whereClause = replaceAllIgnoreCaseExcludeWithinLiteral(whereClause,"<", "lt");
+            whereClause = replaceAllIgnoreCaseExcludeWithinLiteral(whereClause,">", "gt");
+            whereClause = replaceAllIgnoreCaseExcludeWithinLiteral(whereClause, " AND ", " and ");
+            whereClause = replaceAllIgnoreCaseExcludeWithinLiteral(whereClause, " OR ", " or ");
+            whereClause = replaceAllIgnoreCaseExcludeWithinLiteral(whereClause, " NOT ", " not ");
+            whereClause = replaceAllIgnoreCaseExcludeWithinLiteral(whereClause, " IN ", " in ");
 
             // String Functions
             whereClause = transformLikeToODataOperator(whereClause);
@@ -76,8 +72,10 @@ public class ODataUrlBuilder {
         }
     }
 
-    private static Tuple replaceExcludeWithinLiteral(String target, String replaced, String replacement, int index) {
-        int replacedIndex = target.indexOf(replaced, index);
+    // 置換対象文字がシングルクォーテーションの囲みの外(置換対象文字以前のシングルクォーテーションを先頭から数えて偶数個（''は除く））の場合のみ置換する
+    private static Tuple replaceIgnoreCaseExcludeWithinLiteral(String target, String replaced, String replacement, int index) {
+        // case insensitive indexOf
+        int replacedIndex = target.toLowerCase().indexOf(replaced.toLowerCase(), index);
         if (replacedIndex == -1) {
             return new Tuple(replacedIndex, target);
         }
@@ -104,12 +102,13 @@ public class ODataUrlBuilder {
         return new Tuple(replacedIndex, target);
     }
 
-    static String replaceAllExcludeWithinLiteral(String target, String replaced, String replacement) {
-        int replacedCount = target.split(replaced).length;
+    static String replaceAllIgnoreCaseExcludeWithinLiteral(String target, String replaced, String replacement) {
+        // case insensitive split
+        int replacedCount = target.split("(?i)" + replaced).length;
         String result = target;
         int index = 0;
         for (int i = 0; i < replacedCount; i++) {
-            Tuple tuple = replaceExcludeWithinLiteral(result, replaced, replacement, index);
+            Tuple tuple = replaceIgnoreCaseExcludeWithinLiteral(result, replaced, replacement, index);
             index = tuple.index + replacement.length();
             result = tuple.value;
         }
@@ -125,11 +124,7 @@ public class ODataUrlBuilder {
             this.value = value;
         }
     }
-
-    private String replaceAllIgnoreCase(String target, String regex, String replacement) {
-        return Pattern.compile(regex, Pattern.CASE_INSENSITIVE).matcher(target).replaceAll(replacement);
-    }
-
+    
     private String transformLikeToODataOperator(String whereClause) {
         int likeIndex = StringUtil.indexOfIgnoreCase(whereClause, " LIKE ");
         if (likeIndex == -1) {
--- src/test/java/odata_jdbc/jdbc/ODataUrlBuilderTest.java
+++ src/test/java/odata_jdbc/jdbc/ODataUrlBuilderTest.java
@@ -8,11 +8,12 @@ public class ODataUrlBuilderTest {
 
     @Test
     public void testReplaceAllExcludeWithinLiteral() {
-        assertEquals("id eq 'a'", ODataUrlBuilder.replaceAllExcludeWithinLiteral("id = 'a'", "=", "eq"));
-        assertEquals("id eq 'a=b'", ODataUrlBuilder.replaceAllExcludeWithinLiteral("id = 'a=b'", "=", "eq"));
-        assertEquals("id eq 'a=b' or xx eq 'a=b'", ODataUrlBuilder.replaceAllExcludeWithinLiteral("id = 'a=b' or xx = 'a=b'", "=", "eq"));
+        assertEquals("id eq 'a'", ODataUrlBuilder.replaceAllIgnoreCaseExcludeWithinLiteral("id = 'a'", "=", "eq"));
+        assertEquals("id eq 'a=b'", ODataUrlBuilder.replaceAllIgnoreCaseExcludeWithinLiteral("id = 'a=b'", "=", "eq"));
+        assertEquals("id eq 'a=b' or xx eq 'a=b'", ODataUrlBuilder.replaceAllIgnoreCaseExcludeWithinLiteral("id = 'a=b' or xx = 'a=b'", "=", "eq"));
 
-        assertEquals("id eq 'a' and xx eq '1 AND 2'", ODataUrlBuilder.replaceAllExcludeWithinLiteral("id eq 'a' AND xx eq '1 AND 2'", " AND ", " and "));
+        assertEquals("id eq 'a' and xx eq '1 AND 2'", ODataUrlBuilder.replaceAllIgnoreCaseExcludeWithinLiteral("id eq 'a' AND xx eq '1 AND 2'", " AND ", " and "));
+        assertEquals("id eq 'a' and xx eq '1 And 2' and x eq 1", ODataUrlBuilder.replaceAllIgnoreCaseExcludeWithinLiteral("id eq 'a' And xx eq '1 And 2' anD x eq 1", " AND ", " and "));
     }
 
 }
